72d6f7fb6bb9092839214ec5442ea65ae8267867
sdhabal100 <sdhabal100@gmail.com>
2024-01-04 20:56:58 +0530

    127:Create new Feature

---
+++ src/main/java/com/demo/myblog10/Myblog10Application.java
@@ -0,0 +1,13 @@
+package com.demo.myblog10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblog10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblog10Application.class, args);
+	}
+
+}
